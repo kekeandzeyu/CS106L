3c1618fd7eb91b7122388c52c9afb27847ac781c
kekeandzeyu <kekeandzeyu@gmail.com>
2024-11-24 18:48:54 +0800

    update Treebook

--- Treebook/user.cpp
+++ Treebook/user.cpp
@@ -15,29 +15,27 @@ User::User(const std::string& name)
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
  */
-void
-User::add_friend(const std::string& name)
+void User::add_friend(const std::string& name)
 {
-  if (_size == _capacity) {
-    _capacity = 2 * _capacity + 1;
-    std::string* newFriends = new std::string[_capacity];
-    for (size_t i = 0; i < _size; ++i) {
-      newFriends[i] = _friends[i];
+    if (_size == _capacity) {
+        _capacity = 2 * _capacity + 1;
+        std::string* newFriends = new std::string[_capacity];
+        for (size_t i = 0; i < _size; ++i) {
+            newFriends[i] = _friends[i];
+        }
+        delete[] _friends;
+        _friends = newFriends;
     }
-    delete[] _friends;
-    _friends = newFriends;
-  }
 
-  _friends[_size++] = name;
+    _friends[_size++] = name;
 }
 
 /**
  * Returns the name of this User.
  */
-std::string
-User::get_name() const
+std::string User::get_name() const
 {
-  return _name;
+    return _name;
 }
 
 /**
@@ -46,7 +44,7 @@ User::get_name() const
 size_t
 User::size() const
 {
-  return _size;
+    return _size;
 }
 
 /**
@@ -56,10 +54,80 @@ User::size() const
  */
 void User::set_friend(size_t index, const std::string& name)
 {
-  _friends[index] = name;
+    _friends[index] = name;
 }
 
-/** 
- * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
+/**
+ * Print a user with a name and a list of friends (Part 1).
+ * @param os The output stream to print to.
+ *
  */
+std::ostream& operator<<(std::ostream& os, const User& user) {
+    os << "User(name=" << user._name << ", friends=[";
+    for (size_t i = 0; i < user._size; ++i) {
+        os << user._friends[i];
+        if (i < user._size - 1) {
+            os << ", ";
+        }
+    }
+    os << "])";
+    return os;
+}
+
+/**
+ * Destructor for the User class (Part 2).
+ */
+User::~User() {
+    delete[] _friends;
+}
+
+/**
+ * Copy constructor for the User class (Part 2).
+ * @param user The User to copy.
+ */
+User::User(const User& user)
+    : _name(user._name),
+      _friends(new std::string[user._capacity]),
+      _size(user._size),
+      _capacity(user._capacity)
+{
+    for (size_t i = 0; i < _size; ++i) {
+        _friends[i] = user._friends[i];
+    }
+}
+
+/**
+ * Copy assignment operator for the User class (Part 2).
+ * @param user The User to copy.
+ */
+User& User::operator=(const User& user) {
+    if (this != &user) {
+        delete[] _friends;
+        _name = user._name;
+        _size = user._size;
+        _capacity = user._capacity;
+        _friends = new std::string[_capacity];
+        for (size_t i = 0; i < _size; ++i) {
+            _friends[i] = user._friends[i];
+        }
+    }
+    return *this;
+}
+
+/**
+ * += represents adding a user to another user's friend list (Part 3).
+ * @param other The User to add as a friend.
+ */
+User& User::operator+=(User& other) {
+    add_friend(other.get_name());
+    other.add_friend(get_name());
+    return *this;
+}
+
+/**
+ * < represents comparing users alphabetically by name (Part 3).
+ * @param other The User to compare to.
+ */
+bool User::operator<(const User& other) const {
+    return _name < other._name;
+}
--- Treebook/user.h
+++ Treebook/user.h
@@ -9,20 +9,29 @@
 class User
 {
 public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
-  std::string get_name() const;
-  size_t size() const;
-  void set_friend(size_t index, const std::string& name);
+    User(const std::string& name);
+    void add_friend(const std::string& name);
+    std::string get_name() const;
+    size_t size() const;
+    void set_friend(size_t index, const std::string& name);
 
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+    /* Part 1 */
+    friend std::ostream& operator<<(std::ostream& os, const User& user);
+
+    /* Part 2 */
+    ~User(); // Destructor
+    User(const User& user); // Copy constructor
+    User& operator=(const User& user); // Copy assignment operator
+    User(User&& user) = delete; // Delete move constructor
+    User& operator=(User&& user) = delete; // Delete move assignment operator
+
+    /* Part 3 */
+    User& operator+=(User& other);  // Compound assignment operator
+    bool operator<(const User& other) const; // Comparison operator
 
 private:
-  std::string _name;
-  std::string* _friends;
-  size_t _size;
-  size_t _capacity;
+    std::string _name;
+    std::string* _friends;
+    size_t _size;
+    size_t _capacity;
 };
